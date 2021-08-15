a45c94bd3664a81cbe7a501c4717e368164595aa
Dartloli <newkokima@gmail.com>
2021-08-16 00:31:16 +0300

    Added hive database

---
+++ lib/data/db/hive_constants.dart
@@ -0,0 +1,10 @@
+class HiveTypeIds {
+  static const characterLocation = 0;
+  static const character = 1;
+  static const searchRequest = 2;
+}
+
+class HiveBoxNames {
+  static const characters = 'characters';
+  static const searchRequest = 'searchRequests';
+}
